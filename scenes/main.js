09277ea34c7d5cedffe6c5f61171711729788e1f
Ahsan <ahsan.tntboy@gmail.com>
2021-06-21 20:17:17 +0000

    Building Base

--- scenes/main.js
+++ scenes/main.js
@@ -1,22 +1,40 @@
+const MOVE_SPEED=300;
+
 const map=[
-  '                          ',
-  '                   S      ',
-  '                          ',
-  '                          ',
-  '                          ',
-  '                          ',
-  'xxxxxxxxxxxxxxxxxxxxxxxxxx',
-]
+  '             -------------',
+  '                    X     ',
+  '             -------------',
+  '                    X     ',
+  '             -------------',
+  '                    X     ',
+  '             -------------',
+  '                    X     ',
+  '             -------------',]
 const levelCfg={
   height:30,
   width:30,
-  'x':[sprite('ground')],
-  'S':[sprite('sun'),scale(2)],
-  
+  'X':[sprite('answer')],
+  '-':[sprite('lane')],
 }
 const car=add([
-sprite('car'), pos(30, height() / 2),
+sprite('car'), pos(30, height() / 2-3),
   origin('center'),
   solid()
 ])
+
+keyDown('right',()=>{
+  car.move(MOVE_SPEED,0)
+})
+
+keyDown('left',()=>{
+  car.move(-MOVE_SPEED,0)
+})
+
+keyDown('down',()=>{
+  car.move(0,MOVE_SPEED)
+})
+
+keyDown('up',()=>{
+  car.move(0,-MOVE_SPEED)
+})
 addLevel(map,levelCfg)
